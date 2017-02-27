c969848f5b9e5049f6753ccd188496c97a1c2d8d
VLE20 <ervin.vildeman@gmail.com>
2017-02-27 16:19:04 +0100

    Init features

--- app/models/Setting.js
+++ app/models/Setting.js
@@ -1,3 +1,20 @@
 /**
  * Created by TBS on 16/02/2017.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var settingSchema = new Schema(
+    {
+        port: {
+            type: Schema.Types.ObjectId,
+            ref: 'Port'
+        },
+        downloadDestination: String,
+        isActive: Boolean,
+        date: String,
+    }, {timestamps: true} );
+
+
+var Setting = mongoose.model('Setting', settingSchema);
+module.exports = Setting;
