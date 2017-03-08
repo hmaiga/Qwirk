64ac110d9b815b01ceeb5f8f55cea7820bd4bc60
VLE20 <ervin.vildeman@gmail.com>
2017-03-08 10:35:10 +0100

    fix bug : getUsers and others always send all users

--- app/routers/bot.js
+++ app/routers/bot.js
@@ -6,7 +6,7 @@ var botController = require('./../controllers').bot
 var botRouters = function botRouters(router) {
     router.route('/bots')
         .get(function(req, res) {
-            params = req.query.filter ? JSON.parse(req.query.filter) : {}
+            params = req.query ? req.query : {}
             return botController.getBots(params, function(err, bots) {
                 if (err) return res.status(500).send(err)
                 else {
