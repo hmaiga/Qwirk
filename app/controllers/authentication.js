d917e90d5b1fed2dca0b265230c36bb39b212980
Jennyfer NGUENO <jngueno@yahoo.fr>
2017-03-21 16:07:36 +0100

    Resolve user insertion problem

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -1,27 +1,33 @@
-    /**
+/**
  * Created by jngue on 14/03/2017.
  */
-var passport = require('passport');
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+let passport = require('passport');
+let mongoose = require('mongoose');
+let User = mongoose.model('User');
+
+let logger = require("./../helpers/logger");
 
 class authentication {
     static register (req, res) {
-        var user = new User();
-
+        let user = new User();
         user.firstName = req.body.firstName;
         user.lastName = req.body.lastName;
         user.email = req.body.email;
         user.username = req.body.username;
         user.setPassword(req.body.password);
-        user.groups = req.body.groups;
-        user.contacts = req.body.contacts;
-        user.invitedGroups = req.body.invitedGroups;
-        user.status = req.body.status;
-        user.settings = req.body.settings;
-        user.isModerator = req.body.isModerator;
+        user.groups = req.body.groups || [];
+        user.contacts = req.body.contacts || [];
+        user.invitedGroups = req.body.invitedGroups || [];
+        user.status = req.body.status || null;
+        user.settings = req.body.settings || [];
+        user.isModerator = req.body.isModerator || false;
         user.save(function(err) {
-            var token;
+            let token;
+            if (err) {
+                res.status(500).json(err);
+                return;
+            }
+
             token = user.generateJwt();
             res.status(200);
             res.json({
@@ -29,10 +35,9 @@ class authentication {
             });
         });
     }
-
     static login (req, res) {
         passport.authenticate('local', function(err, user, info){
-            var token;
+            let token;
 
             // If Passport throws/catches an error
             if (err) {
